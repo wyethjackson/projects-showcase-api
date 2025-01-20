a4214c71512708cc02c7520a0cc705fd57342285
Wyeth Jackson <wyethjackson@gmail.com>
2025-01-20 15:44:08 -0500

    added migration feature

--- src/db.rs
+++ src/db.rs
@@ -2,24 +2,42 @@ use std::fs;
 use std::io::Error as IOError;
 use postgres::{Client, NoTls, Error as PgError};
 use dotenvy::dotenv;
-use std::env;
+use std::{env, path::Path};
 
 pub fn connect_db() -> Result<Client, PgError> {
     dotenv().ok();
     
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env");
-    let client = Client::connect(&database_url, NoTls)?;
-
+    let mut client = Client::connect(&database_url, NoTls)?;
+    apply_migrations(&mut client)?;
     Ok(client)
 }
 
-// Note: not used yet, for use when building out scalable db arch
+fn apply_migrations(client: &mut Client) -> Result<(), PgError> {
+    let migrations_path = Path::new("src/sql/migrations");
+
+    if migrations_path.exists() {
+        for entry in fs::read_dir(migrations_path).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.extension().unwrap_or_default() == "sql" {
+                let sql = fs::read_to_string(&path).unwrap();
+                client.batch_execute(&sql)?;
+                info!("Migration executed: {:?}", path.file_name().unwrap());
+            }
+        }
+    } else {
+        info!("No migrations folder found");
+    }
+    Ok(())
+}
+
 pub fn load_query(file_path: &str) -> Result<String, IOError> {
     match fs::read_to_string(file_path) {
         Ok(q) => Ok(q),
         Err(e) => {
             error!("Failed to load SQL query: {}", e);
-            Err(e) // ✅ Return `std::io::Error`, not `Status`
+            Err(e)
         }
     }
 }
